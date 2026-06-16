31338dcd7cf6292e39eae58dd907a5529961d6cb
Alan Bishop <alan_bishop@yahoo.com>
2026-06-16 13:00:31 +0100

    Remove duplicate header from pillow questionnaire page

--- app/pillow/questionnaire/page.tsx
+++ app/pillow/questionnaire/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { categoryRegistry } from "../../../config/registry";
 import QuestionnairePage from "../../questionnaire/page-client";
-import { Header } from "../../../components/Header";
 
 export default function PillowQuestionnairePage() {
   const config = categoryRegistry["pillow"];
   return (
-    <>
-      <Header />
-      <QuestionnairePage
-        questionnaire={config.questionnaire}
-        resultsPath="/pillow/results"
-      />
-    </>
+    <QuestionnairePage
+      questionnaire={config.questionnaire}
+      resultsPath="/pillow/results"
+    />
   );
 }
